a3c845873cccc404dd21370c24b5bba2442b1dc6
Blo2SonNT <migustanieto.2@gmail.com>
2022-08-18 21:42:14 -0500

    paginacion de rick and morty - appendchild

--- API/assets/js/code.js
+++ API/assets/js/code.js
@@ -1,19 +1,51 @@
-let api_rm = "https://rickandmortyapi.com/api/character"
-let consumo = fetch(api_rm)
-consumo.then(res_gatito => res_gatito.json())
-    .then((data_api) => {
-        console.log(data_api)
-        for (const personajes_api of data_api.results) {
-            document.querySelector("#cartas-personajes").innerHTML += `
-            <div class="col">
-                <div class="card">
-                    <img src="${personajes_api.image}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${personajes_api.name}</h5>
-                        <p class="card-text">${personajes_api.species}</p>
+function consumo_api_total(url_a_consumir) {
+    // let api_rm = "https://rickandmortyapi.com/api/character?page=32"
+    let consumo = fetch(url_a_consumir)
+    consumo.then(res_gatito => res_gatito.json())
+        .then((data_api) => {
+            console.log(data_api)
+            document.querySelector("#cartas-personajes").innerHTML = ''
+            for (const personajes_api of data_api.results) {
+                document.querySelector("#cartas-personajes").innerHTML += `
+                <div class="col">
+                    <div class="card">
+                        <img src="${personajes_api.image}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${personajes_api.name}</h5>
+                            <p class="card-text">${personajes_api.species}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
             `
-        }
-    })
+            }
+            crear_botones(data_api.info.next, data_api.info.prev)
+        })
+}
+
+function crear_botones(url_pagina_siguiente, url_pagina_anterior) {
+    let div_paginacion = document.querySelector("#paginacion")
+    div_paginacion.innerHTML = ''
+    let boton_siguiente = document.createElement("button")
+    boton_siguiente.className = "btn btn-dark"
+        // boton_siguiente.classList.add("btn", "btn-dark")
+    boton_siguiente.innerText = "Siguiente pagina"
+    if (url_pagina_siguiente != null) {
+        boton_siguiente.setAttribute("onclick", `consumo_api_total('${url_pagina_siguiente}')`)
+    } else {
+        boton_siguiente.setAttribute("disabled", '')
+    }
+    div_paginacion.appendChild(boton_siguiente)
+
+
+    let boton_anterior = document.createElement("button")
+    boton_anterior.classList.add("btn", "btn-primary")
+    boton_anterior.innerText = "Pagina anterior"
+    if (url_pagina_anterior != null) {
+        boton_anterior.setAttribute("onclick", `consumo_api_total('${url_pagina_anterior}')`)
+    } else {
+        boton_anterior.setAttribute("disabled", '')
+    }
+    div_paginacion.appendChild(boton_anterior)
+}
+
+consumo_api_total("https://rickandmortyapi.com/api/character")
