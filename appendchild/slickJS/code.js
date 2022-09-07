db59a953eb520300c2a3801d7b14ca28f041296c
Blo2SonNT <migustanieto.2@gmail.com>
2022-09-06 21:00:05 -0500

    mongo

---
+++ appendchild/slickJS/code.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.slider1Paramount').slick({
+        infinite: false,
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        arrows: true,
+    });
+});
