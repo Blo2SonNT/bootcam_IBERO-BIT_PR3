878eac6726acf879f20c88f7f37affa5fd8cc6d9
Blo2SonNT <migustanieto.2@gmail.com>
2022-09-20 18:55:26 -0500

    angular - formularios reactivos

--- angular_base/proyectointroduccion/src/app/app.module.ts
+++ angular_base/proyectointroduccion/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ContactoComponent } from './components/contacto/contacto.component';
 import { IngresoComponent } from './components/ingreso/ingreso.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular_base/proyectointroduccion/src/app/components/registro/registro.component.ts
+++ angular_base/proyectointroduccion/src/app/components/registro/registro.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-registro',
-  templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+    selector: 'app-registro',
+    templateUrl: './registro.component.html',
+    styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+    registroForm: FormGroup;
+    regexCorreo = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
+    // regexPass = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/
 
-  ngOnInit(): void {
-  }
+    constructor(private fb: FormBuilder) {
+        this.registroForm = this.fb.group({
+            nombre: ['', Validators.required],
+            correo: ['', [Validators.required, Validators.pattern(this.regexCorreo)]],
+            clave: ['', [Validators.required]],//Validators.pattern(this.regexPass)
+            VericarClave: ['', Validators.required],
+        })
+    }
+
+    ngOnInit(): void {
+    }
+
+    registrarUsurio(){
+        console.log(this.registroForm.get('clave')?.value)
+        console.log(this.registroForm.get('VericarClave')?.value)
+        let varClave = this.registroForm.get('clave')?.value
+        let verificar = this.registroForm.get('VericarClave')?.value
+        if(varClave != verificar){
+            console.error("Las contraseñas no coinciden")
+        }
+    }
+
+
 
 }
